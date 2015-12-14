4f798fa38a14f5d3728e7fbdfefdd46dd07677cf
Victor Oliveira <victordext@gmail.com>
2015-12-14 11:43:47 -0300

    Code Author short update

--- LevelManager.cs
+++ LevelManager.cs
@@ -1,4 +1,17 @@
-﻿using UnityEngine;
+﻿/*                        NUMBER WIZARD UI							  */
+/*		                                                              */
+/*																	  */
+/*		This is the code from a game created to practice my           */
+/*      Unity and Game Development skills. All of this was            */
+/*      made watching Ben Tristem's tutorial at Udemy.                */
+/*                                                                    */
+/*      For more information please visit:                            */
+/*      https://www.udemy.com/unitycourse/                            */
+/*                                                                    */
+/*                                                                    */
+/**********************************************************************/
+
+using UnityEngine;
 using System.Collections;
 
 public class LevelManager : MonoBehaviour {
